115407b23b9ac2ef1504f4f9e7eba631b8765f55
CanmingCai <caicanming50@gmail.com>
2022-04-22 12:46:31 -0500

    envio correo

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,3 +1,4 @@
+
 function contactos(){
     nombre = document.f_contac.nom.value;
     correo = document.f_contac.correo.value;
@@ -6,6 +7,8 @@ function contactos(){
     ciudad = document.f_contac.ciudad.value;
     codigo = document.f_contac.c_postal.value;
     pais = document.f_contac.pais.value;
+    
+    body = "<h1><font color=orange>Nueva formulario</font></h1> <br/>" + "Nombre : " + nombre + "<br/> Email : " + correo  + "<br/> Mensaje : " + mensaje + "<br/> Ciudad : " + ciudad + "<br/> Codigo : " + codigo + "<br/> Pais : " + pais;
 
     var sw = correo.indexOf("@", 0); 
     var sw1 = correo.indexOf(".", 0);
@@ -17,7 +20,30 @@ function contactos(){
         if (nombre=="") document.f_contac.nom.style.border = "1px solid red";
             else document.f_contac.nom.style.border = "1px solid green";
         if (correo=="") document.f_contac.correo.style.border = "1px solid red";
+            if (correo.indexOf("@", 0) == -1) { 
+                alert("El correo tiene que llervar @") 
+                document.f_contac.correo.style.border = "1px solid red";
+                } 
             else document.f_contac.correo.style.border = "1px solid green";
+            
+            if (correo.indexOf(".", 0) == -1) { 
+                alert("El correo tiene que llevar .")
+                document.f_contac.correo.style.border = "1px solid red";
+                } 
+            else document.f_contac.correo.style.border = "1px solid grenn";
+            
+            if (tt == 1) { 
+                alert("@ y . no pueden estar junto");
+                document.f_contac.correo.style.border = "1px solid red";
+                }
+            else document.f_contac.correo.style.border = "1px solid green";
+            
+            if (sw > sw1) { 
+                alert("@ tiene que estar enfrente de . ")
+                document.f_contac.correo.style.border = "1px solid red";
+                } 
+            else document.f_contac.correo.style.border = "1px solid red";
+
         if (asunto=="") document.f_contac.asunto.style.border = "1px solid red";
             else document.f_contac.asunto.style.border = "1px solid green";
         if (mensaje=="") document.f_contac.mensaje.style.border = "1px solid red";
@@ -28,37 +54,34 @@ function contactos(){
             else document.f_contac.c_postal.style.border = "1px solid green";
         if (pais=="") document.f_contac.pais.style.border = "1px solid red";
             else document.f_contac.pais.style.border = "1px solid green";
-
-        if (correo.indexOf("@", 0) == -1) { 
-            alert("El correo tiene que llervar @") 
-            document.f_contac.correo.style.border = "1px solid red";
-            } 
-            else document.f_contac.correo.style.border = "1px solid green";
-        
-        if (correo.indexOf(".", 0) == -1) { 
-            alert("El correo tiene que llevar .")
-            document.f_contac.correo.style.border = "1px solid red";
-            } 
-            else document.f_contac.correo.style.border = "1px solid green";
-        
-        if (tt == 1) { 
-            alert("@ y . no pueden estar junto");
-            document.f_contac.correo.style.border = "1px solid red";
-            }
-            else document.f_contac.correo.style.border = "1px solid green";
-        
-        
-        if (sw > sw1) { 
-            alert("@ tiene que estar enfrente de . ")
-            document.f_contac.correo.style.border = "1px solid red";
-            } 
-            else document.f_contac.correo.style.border = "1px solid green";
+    
     }
+
+    
     else{
-		url="contacto.php?nombre="+nombre+"&correo="+correo+"&asunto="+asunto+"&mensaje="+mensaje+"&ciudad="+ciudad+"&codigo="+codigo+"&pais="+pais;
-		location.href=url;
+		//url="contacto.php?nombre="+nombre+"&correo="+correo+"&asunto="+asunto+"&mensaje="+mensaje+"&ciudad="+ciudad+"&codigo="+codigo+"&pais="+pais;
+		//location.href=url;
         //alert(url);
+        document.f_contac.nom.style.border = "1px solid green";
+        document.f_contac.correo.style.border = "1px solid green";
+        document.f_contac.asunto.style.border = "1px solid green";
+        document.f_contac.mensaje.style.border = "1px solid green";
+        document.f_contac.ciudad.style.border = "1px solid green";
+        document.f_contac.c_postal.style.border = "1px solid green";
+        document.f_contac.pais.style.border = "1px solid green";
+
+        Email.send({
+            text : "quiero 5",
+            Host : "smtp.gmail.com",
+            Username : "287434445cai@gmail.com",
+            Password : "kqusuoalgoyejdqb",
+            To : '287434445cai@gmail.com',
+            From :correo,
+            Subject : asunto,
+            Body : body
+        }).then(
+          alert("Correo enviado!"));
 	}
-
-
 }
+
+
